680404e014e54cc997ce03fa2fd0693ffe36d370
Cristhian Butron <cristhian.butron@ucb.edu.bo>
2025-05-30 19:05:20 -0400

    cambios para hacer merge

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,16 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome do |app|
+Capybara.register_driver :chrome_incognito do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--incognito')
+  options.add_argument('--disable-infobars')
+  options.add_argument('--disable-notifications')
+  options.add_argument('--disable-extensions')
+  options.add_argument('--start-maximized')
+
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.default_driver = :selenium_chrome
-Capybara.default_max_wait_time = 10
+Capybara.default_driver = :chrome_incognito
+Capybara.javascript_driver = :chrome_incognito
