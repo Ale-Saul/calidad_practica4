2da154f6dffbfc2a115d56d1bc3ee0d380f87775
Alejandro <alejandro.mont.cesp@gmail.com>
2025-05-30 15:27:52 -0400

    Se cambio a una ventana privada para que no muestre el error de contraeña insegura

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,16 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+Capybara.register_driver :chrome_incognito do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--incognito')
+  options.add_argument('--disable-infobars')
+  options.add_argument('--disable-notifications')
+  options.add_argument('--disable-extensions')
+  options.add_argument('--start-maximized')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.default_driver = :selenium_chrome
-Capybara.default_max_wait_time = 5 
+Capybara.default_driver = :chrome_incognito
+Capybara.javascript_driver = :chrome_incognito
